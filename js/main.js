a1328f0a8c420d103a235ad7ac64dc1ab1303afb
Art <semitro_8@mail.ru>
2019-09-22 14:30:44 +0300

    normal cube is rotating

--- js/main.js
+++ js/main.js
@@ -54,93 +54,100 @@ function main() {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(objectIndicies), gl.STATIC_DRAW)
 
+  var time = Date.now();
+  function mainLoop(){
+    const dt = Date.now() - time;
+    time += dt;
+    gl.clearColor(0., 0., 0., 0.05);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL); // obucre far things
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    const modelViewMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0, -6.0]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, time*0.0005, [0, 1, 1]);
+
+    // pos
+    const vertexPos = program_info.attribLocations.vertexPos;
+    gl.bindBuffer(gl.ARRAY_BUFFER, boxBuffer);
+    gl.vertexAttribPointer(
+      vertexPos, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertexPos);
+
+    // color
+    const vertexColor = program_info.attribLocations.vertexColor;
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(vertexColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertexColor);
+
+    // indices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBuffer);
+    //
+    const projectionMatrixUniform = program_info.uniformLocations.projectionMatrix;
+    const modelViewMatrixUniform = program_info.uniformLocations.modelViewMatrix;
+    gl.uniformMatrix4fv(projectionMatrixUniform, false, projectionMatrix);
+    gl.uniformMatrix4fv(modelViewMatrixUniform, false, modelViewMatrix);
+
+    gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0); // run frag shader for each vert
+  }
+  setInterval(mainLoop, 0);
   // drawing
-  gl.clearColor(0., 0., 0., 0.05);
-  gl.clearDepth(1.0);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL); // obucre far things
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  const fieldOfView = 45 * Math.PI / 180;
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-  const modelViewMatrix = mat4.create();
-
-  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, -1.5, -6.0]);
-  mat4.rotate(modelViewMatrix, modelViewMatrix, 0.5, [0, 1, 1]);
-
-  // pos
-  const vertexPos = program_info.attribLocations.vertexPos;
-  gl.vertexAttribPointer(
-    vertexPos, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vertexPos);
-
-  // color
-  const vertexColor = program_info.attribLocations.vertexColor;
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.vertexAttribPointer(vertexColor, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vertexColor);
-
-  // indices
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBuffer);
-  //
-  const projectionMatrixUniform = program_info.uniformLocations.projectionMatrix;
-  const modelViewMatrixUniform = program_info.uniformLocations.modelViewMatrix;
-  gl.uniformMatrix4fv(projectionMatrixUniform, false, projectionMatrix);
-  gl.uniformMatrix4fv(modelViewMatrixUniform, false, modelViewMatrix);
-
-  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0); // run frag shader for each vert
 }
 
 function getObject() {
   const positions = [
     // Front face
-    -1.0, -1.0, 1.0,
-    1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0,
-    -1.0, 1.0, 1.0,
+    -1.0, -1.0,  1.0,
+    1.0, -1.0,  1.0,
+    1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
 
     // Back face
     -1.0, -1.0, -1.0,
-    -1.0, 1.0, -1.0,
-    1.0, 1.0, -1.0,
+    -1.0,  1.0, -1.0,
+    1.0,  1.0, -1.0,
     1.0, -1.0, -1.0,
 
     // Top face
-    -1.0, 1.0, -1.0,
-    -1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0,
-    1.0, 1.0, -1.0,
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+    1.0,  1.0,  1.0,
+    1.0,  1.0, -1.0,
 
     // Bottom face
     -1.0, -1.0, -1.0,
     1.0, -1.0, -1.0,
-    1.0, -1.0, 1.0,
-    -1.0, -1.0, 1.0,
+    1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
 
     // Right face
     1.0, -1.0, -1.0,
-    1.0, 1.0, -1.0,
-    1.0, 1.0, 1.0,
-    1.0, -1.0, 1.0,
+    1.0,  1.0, -1.0,
+    1.0,  1.0,  1.0,
+    1.0, -1.0,  1.0,
 
     // Left face
     -1.0, -1.0, -1.0,
-    -1.0, -1.0, 1.0,
-    -1.0, 1.0, 1.0,
-    -1.0, 1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0,
   ];
   const indices = [
-    0, 1, 2, 0, 2, 3,    // front
-    4, 5, 6, 4, 6, 7,    // back
-    8, 9, 10, 8, 10, 11,   // top
-    12, 13, 14, 12, 14, 15,   // bottom
-    16, 17, 18, 16, 18, 19,   // right
-    20, 21, 22, 20, 22, 23,   // left
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23,   // left
   ];
   const faceColors = [
     [1.0, 1.0, 1.0, 1.0],    // Front face: white
