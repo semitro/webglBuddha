3db6de4b83ee525019c5439ccd9a66758a29dcb3
Art <semitro_8@mail.ru>
2019-09-23 19:31:33 +0300

    car is shown

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,16 @@
 cp -R . /var/www/html 2>/dev/null
 cd /var/www/html/js || echo "Can't cd to /var" >&2
 
+# use C preprocessor on js files to use include macro in them
 preprocess() {
   filename="$1"
   mv "$filename.js" "$filename.c"
-  gcc -E --no-warnings main.c | sed '/^#.*/d' > "$filename.js"
-
+  gcc -E --no-warnings "$filename.c" | sed '/^#.*/d' > "$filename.js"
+  rm "$filename.c"
 }
 
+preprocess model
+preprocess render
+preprocess main
+
+echo "$0 done"
